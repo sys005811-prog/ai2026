d013fcdb48cec902064b7622126bb20f8a0f694b
sys005811-prog <sys200331@naver.com>
2026-03-25 16:30:36 +0900

    강의중,

--- 0325-1.py
+++ 0325-1.py
@@ -1,8 +1,14 @@
 #for:반복횟수, while:반복조건
+
 for i in range(5):   #0~4, 5개의 숫자를 생성, range(1,5)일 경우 1,2,3,4 출력
     print(i)
 for i in range(6):
-    print( ':welcome to everyone!')
-    
+    print( i,':welcome to everyone!')
+
+
+#range( A, B ,C) A:초기값, B:종료값 (반복조건<종료값 일 때), C:증가값(간격)
+
+for i in range(1,9,2):
+    print (i)
 
     
