904e8acc493457d0ab9dfa96035c9d43ff479981
sys005811-prog <sys200331@naver.com>
2026-03-25 16:44:38 +0900

    0325강의

--- 0325-1(반복문).py
+++ 0325-1(반복문).py
@@ -10,5 +10,3 @@ for i in range(6):
 
 for i in range(1,9,2):
     print (i)
-
-    
---
+++ 0325-2(리스트).py
@@ -0,0 +1,26 @@
+
+score=(87, 84, 95, 67, 88, 94, 63)
+name =('김','박','이')
+print(score)
+print(type(score))
+
+for i in score:
+    print(i)
+
+for i in name:
+    print(i)
+
+for J in score:  #i,J는 모두 변수 이름, 변수 이름을 바꿔도 같은 함수임
+    print(J)
+'''
+'''
+score=(87, 84, 95, 67, 88, 94, 63)
+name =('김','박','이',score)
+add=(('김XX',24,'사람아님'),('박XX',27, '개백수'),('이XX',21,'원숭이'))
+
+for i in add:
+    print(i)
+'''
+'''
+ri=list(range(5))   #list 변수화 
+print(ri)
