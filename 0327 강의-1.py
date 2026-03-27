11ab48754b12bd4a4fc418318b646d1a69acef9c
sys005811-prog <sys200331@naver.com>
2026-03-27 10:17:07 +0900

    0327강의

---
+++ 0327 강의-1.py
@@ -0,0 +1,17 @@
+
+# end: 끝,() 안에 기호 삽입, 내용 없을시 \(줄바꿈) 자동 삽입  
+for i in range(5):
+
+    print(i, end = ' ')   줄바꿈 
+
+    print(i, end = '\n')  줄바꿈
+
+    print(i)
+
+for i in range(-2, -10, -2):
+    print(i, end = ' ')
+
+    
+
+   
+
